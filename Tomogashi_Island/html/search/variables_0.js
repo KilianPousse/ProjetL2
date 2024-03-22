fb75b199ecc4d06ff19087ff9d1b0769e5919f43
AlbanVallee <157494320+AlbanVallee@users.noreply.github.com>
2024-03-22 14:03:22 +0100

    Delete Tomogashi_Island

--- Tomogashi_Island/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dir_0',['dir',['../structplayer__t.html#a891d4884151019a2190987925120548b',1,'player_t']]]
-];
--- Tomogashi_Island/include/style.h
+++
@@ -1,9 +0,0 @@
-#define IMG_ICON "bin/assets/icon.png"
-
-#define WHITE_COLOR {0xFF, 0xFF, 0xFF, 0xFF}
-#define BLACK_COLOR {0x00, 0x00, 0x00, 0xFF}
-
-#define FULL_WHITE_COLOR {0xFF, 0xFF, 0xFF}
-#define FULL_BLACK_COLOR {0x00, 0x00, 0x00}
-
-#define IMG_SPRITE "bin/assets/textures/sprites/haric"
