dcfce269c174b4d6a11f83ea4ee1149d4ef335c2
AlbanVallee <157494320+AlbanVallee@users.noreply.github.com>
2024-03-27 10:25:02 +0100

    hotbar suite

---
+++ style.h
@@ -0,0 +1,42 @@
+#define IMG_ICON "assets/icon.png"
+/*
+#define WHITE_COLOR {0xFF, 0xFF, 0xFF, 0xFF}
+#define BLACK_COLOR {0x00, 0x00, 0x00, 0xFF}
+*/
+
+#define FULL_WHITE_COLOR {0xFF, 0xFF, 0xFF}
+#define FULL_BLACK_COLOR {0x00, 0x00, 0x00}
+
+#define WHITE_COLOR {0xF3, 0xF4, 0xE7}
+
+#define IMG_SPRITES "assets/textures/sprites"
+
+
+
+//gui
+#define IMG_GUI_HOUR "assets/textures/gui/hour.png"
+#define IMG_GUI_DIALOG "assets/textures/gui/dialog.png"
+
+
+#define PATH_SPRITE_HARICOT "assets/textures/sprites/haricot"
+
+#define PATH_BACKGROUND_OVERLAY "assets/textures/background/overlay_"
+#define PATH_BACKGROUND "assets/textures/background/"
+
+#define IMG_ACTION_EXCLAMATION "assets/textures/actions/exclamation.png"
+#define IMG_ACTION_DIALOGUE "assets/textures/actions/dialogue.png"
+
+
+#define FONT_HOUR "assets/font/small_pixel-7.ttf"
+#define FONT_DIALOG "assets/font/small_pixel-7.ttf"
+
+//hotbar
+
+#define ITEM1 "assets/textures/item/champB1.png"
+#define ITEM2 "assets/textures/item/FleurB1.png"
+#define ITEM3 "assets/textures/item/FleurO2.png"
+#define ITEM4 "assets/textures/item/FleurR2.png"
+#define ITEM5 "assets/textures/item/ItemOF.png"
+#define ITEM6 "assets/textures/item/PousseO3.png"
+
+#define IMGHOTBAR "assets/textures/hotbar/hotbar.png"
