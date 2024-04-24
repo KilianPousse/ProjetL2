cd77b61ae32752754cf27e98bbb559c571911cfd
elonapierre <157494469+elonapierre@users.noreply.github.com>
2024-04-24 11:17:00 +0200

    Ajoue doxy

--- include/fishing.h
+++ include/fishing.h
@@ -1,3 +1,12 @@
+/**
+ * \file fishing.h
+ * \brief fonction qui se trouve dans fishing.c
+ * \details Fontion qui se trouve dans fishing.c
+ * \author Pousse Kilian - Pierre Elona - Vallee Alban 
+ * \version alpha 2.2
+ * \date 15/04/2024
+**/
+
 int main_fishing();
 long fishing_waitTime();
 item_t fishing_fishtype();
